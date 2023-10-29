84116ece525f1f2bd1190196c65cf26c080f6847
Diego Bravo <A2270193@correo.uia.mx>
2023-10-29 13:54:13 -0600

    Se completaron los queries y el procedimiento que me solicitaron en una práctica de base de datos. Cualquier actualización nueva será directactamente reportada.

---
+++ BaseDeDatos/Prácticas/PR1/procedimiento.sql
@@ -0,0 +1,28 @@
+-- PROCEDIMIENTO QUE RECIBE COMO PARÁMETRO EL NOMBRE DEL AJUSTADOR Y UNA FECHA Y DEVUELVE EL VEHÍCULO QUE MANEJÓ EL AJUSTADOR'
+
+DROP PROCEDURE IF EXISTS mostrarVehiculo;
+
+DELIMITER $
+
+CREATE PROCEDURE mostrarVehiculo(IN nom VARCHAR(100), IN tiempo DATE, OUT model VARCHAR(50))
+
+BEGIN
+    DECLARE salida INT;
+    DECLARE curs CURSOR FOR SELECT modelo FROM pr1_vista_AjustadoresFechasModelos WHERE nombre_completo LIKE CONCAT('%', nom, '%') AND fecha = tiempo;
+    DECLARE EXIT HANDLER FOR NOT FOUND SET salida = 1;
+
+    OPEN curs;
+
+    IF salida = 1 THEN
+        SET model = 'No manejó ningún vehículo';
+    END IF;
+
+    FETCH curs INTO model;
+
+    WHILE model != NULL DO 
+        FETCH curs INTO model;
+    END WHILE; 
+
+END$
+
+DELIMITER ;
---
+++ BaseDeDatos/Prácticas/PR1/queries.sql
@@ -0,0 +1,47 @@
+-- USUARIOS QUE MENOS USAN EL SISTEMA --
+
+SELECT CONCAT(nombre, ' ', ap_paterno, ' ', ap_materno) AS nombre FROM pr1_usuarios LEFT JOIN pr1_siniestros USING(idUsuario) GROUP BY nombre HAVING COUNT(idSiniestro) = (SELECT COUNT(idSiniestro) FROM pr1_usuarios LEFT JOIN pr1_siniestros USING(idUsuario) GROUP BY nombre ORDER BY COUNT(idSiniestro) ASC LIMIT 1);
+
+-- AJUSTADORES QUE MÁS SINIETROS ATIENDEN --
+
+SELECT CONCAT(nombre, ' ', ap_paterno, ' ', ap_materno) AS nombre FROM pr1_ajustadores LEFT JOIN pr1_siniestros USING(idAjustador) GROUP BY nombre HAVING COUNT(idSiniestro) = (SELECT COUNT(idSiniestro) FROM pr1_ajustadores LEFT JOIN pr1_siniestros USING(idAjustador) GROUP BY nombre ORDER BY COUNT(idSiniestro) DESC LIMIT 1);
+
+-- AJUSTADORES QUE MENOS SINIESTROS ATIENDEN --
+
+SELECT CONCAT(nombre, ' ', ap_paterno, ' ', ap_materno) AS nombre FROM pr1_ajustadores LEFT JOIN pr1_siniestros USING(idAjustador) GROUP BY nombre HAVING COUNT(idSiniestro) = (SELECT COUNT(idSiniestro) FROM pr1_ajustadores LEFT JOIN pr1_siniestros USING(idAjustador) GROUP BY nombre ORDER BY COUNT(idSiniestro) ASC LIMIT 1);
+
+-- LISTA DE AJUSTADORES REGISTRADOS EN EL SISTEMA --
+
+SELECT idAjustador, CONCAT(nombre, ' ', ap_paterno, ' ', ap_materno) AS nombre FROM pr1_ajustadores;
+
+-- LISTA DE SINIESTROS DE UN AJUSTADOR EN PARTICULAR --
+
+SELECT CONCAT(nombre, ' ', ap_paterno, ' ', ap_materno, ' ') AS nombre, idSiniestro FROM pr1_ajustadores LEFT JOIN pr1_siniestros USING(idAjustador) HAVING nombre LIKE '%Vera%';
+
+-- LISTA DE AJUSTADORES QUE MANEJARON UN VEHÍCULO EN UN PERIODO DE TIEMPO --
+
+SELECT CONCAT(nombre, ' ', ap_paterno, ' ', ap_materno) AS nombre FROM pr1_ajustadores LEFT JOIN pr1_A_V USING(idAjustador) WHERE hora_inicio > '17:00:00' AND hora_fin < '21:00:00';
+
+-- LISTA DE COLONIAS DONDE MÁS SINIESTROS OCURREN --
+
+SELECT colonia FROM pr1_colonias LEFT JOIN pr1_siniestros USING(idColonia) GROUP BY colonia HAVING COUNT(idSiniestro) = (SELECT COUNT(idSiniestro) FROM pr1_colonias LEFT JOIN pr1_siniestros USING(idColonia) GROUP BY colonia ORDER BY COUNT(idSiniestro) DESC LIMIT 1);
+
+-- PERIODO DEL DÍA DONDE MÁS SINIESTROS OCURREN --
+
+SELECT HOUR(hora) AS hora FROM pr1_siniestros GROUP BY hora HAVING COUNT(idSiniestro) = (SELECT COUNT(idSiniestro) FROM pr1_siniestros GROUP BY hora ORDER BY COUNT(idSiniestro) DESC LIMIT 1);
+
+-- CANTIDAD DE SINIESTROS QUE OCURREN EN CIERTO PERIODO DE TIEMPO --
+
+SELECT COUNT(idSiniestro) AS cantidad_siniestros FROM pr1_siniestros WHERE hora > '11:00:00' AND hora < '19:00:00';
+
+-- NOMBRE DEL AJUSTADOR (O AJUSTADORES) QUE MANEJÓ UN VEHÍCULO EN UN PERIODO DE TIEMPO DETERMINADO --
+
+SELECT CONCAT(nombre, ' ', ap_paterno, ' ', ap_materno) AS nombre FROM pr1_ajustadores LEFT JOIN pr1_A_V USING(idAjustador) LEFT JOIN pr1_vehiculos USING(idVehiculo) WHERE hora_inicio  > '10:00:00' AND hora_fin < '14:00:00';
+
+-- ÚLTIMA POSICIÓN DE UN AJUSTADOR EN PARTICULAR --
+
+SELECT CONCAT(nombre, ' ', ap_paterno, ' ', ap_materno) AS nombre, colonia FROM pr1_ajustadores LEFT JOIN pr1_A_V USING(idAjustador) LEFT JOIN pr1_vehiculos USING(idVehiculo) LEFT JOIN pr1_colonias USING(idColonia) WHERE fecha = '2023-11-03';
+
+-- LISTA DE VEHÍCULOS QUE MANEJÓ UN AJUSTADOR EN UN TIEMPO EN ESPECÍFICO --
+
+SELECT modelo FROM pr1_ajustadores LEFT JOIN pr1_A_V USING(idAjustador) LEFT JOIN pr1_vehiculos USING(idVehiculo) WHERE nombre LIKE 'Leonardo' AND ap_paterno = 'Park' AND ap_materno = 'Morales' AND fecha = '2023-09-15';
