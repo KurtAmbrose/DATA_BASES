aacb86b013b723fc350ae526279dd36b8f4a821b
Diego Bravo <A2270193@correo.uia.mx>
2023-11-06 14:13:13 -0600

    Se agregó el procedimiento que hace la validación en el query para buscar si la colonia existe.

--- BaseDeDatos/Prácticas/PR2/funciones.sql
+++ BaseDeDatos/Prácticas/PR2/funciones.sql
@@ -1,1 +1,24 @@
 -- PROCEDIMIENTO QUE AYUDA A VALIDAR SI UN DATO EXISTE ---
+
+DROP PROCEDURE IF EXISTS validarColonia;
+
+DELIMITER $
+
+CREATE PROCEDURE validarColonia(IN dato VARCHAR(50), OUT validacion INT)
+
+BEGIN
+    DECLARE id, salida INT;
+    DECLARE cur CURSOR FOR SELECT idColonia FROM pr1_colonias WHERE colonia = dato;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET salida = 1;
+
+    OPEN cur;
+    FETCH cur INTO id;
+
+    IF salida = 1 THEN
+        SET validacion = 1;
+    ELSE
+        SET validacion = 0;
+    END IF;
+END$
+
+DELIMITER ;
